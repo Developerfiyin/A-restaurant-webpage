5d975fb62950a1e87aae2268c4eceaa884d78fb7
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-04-11 17:46:06 +0100

    updated script.js

--- script.js
+++ script.js
@@ -57,3 +57,13 @@ ScrollReveal().reveal(".header__image__footer", {
   ...scrollRevealOption,
   delay: 4000,
 });
+
+ScrollReveal().reveal(".menu__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".menu__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
